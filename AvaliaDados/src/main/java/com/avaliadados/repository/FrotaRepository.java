c66cf64aad41da2d97252ff8cfd7a8c195c1a104
GugsFranca <guto602@gmail.com>
2025-05-09 14:01:17 -0300

    Adicionando calculo de parametros nas colaboradores no projeto e melhorando o armazenamento dos medicos

--- AvaliaDados/src/main/java/com/avaliadados/repository/FrotaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.avaliadados.repository;
-
-import com.avaliadados.model.FrotaEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FrotaRepository extends JpaRepository<FrotaEntity, String> {
-}
--- AvaliaDados/src/main/java/com/avaliadados/repository/TarmRepository.java
+++
@@ -1,7 +0,0 @@
-package com.avaliadados.repository;
-
-import com.avaliadados.model.TarmEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TarmRepository extends JpaRepository<TarmEntity, String> {
-}
