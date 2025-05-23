aaf2b27140554dd47c585e5ba969d988b5ca2abb
GugsFranca <guto602@gmail.com>
2025-05-23 09:49:55 -0300

    refactor: remove validation annotations from FrotaEntity and TarmEntity constructors for simplified parameter handling

--- AvaliaDados/src/main/java/com/avaliadados/model/FrotaEntity.java
+++ AvaliaDados/src/main/java/com/avaliadados/model/FrotaEntity.java
@@ -5,8 +5,6 @@ import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @Getter
@@ -21,13 +19,11 @@ import lombok.*;
 public class FrotaEntity extends CollaboratorEntity {
 
     public FrotaEntity(
-            @NotBlank(message = "Campo nome é obrigatorio") String nome,
-            @Pattern(message = "CPF fornecido não existe",
-                    regexp = "([0-9]{2}[.]?[0-9]{3}[.]?[0-9]{3}/?[0-9]{4}-?[0-9]{2})")
-            @NotBlank(message = "Campo CPF é obrigatorio") String cpf,
-            @NotBlank(message = "Campo ID Call Rote é obrigatorio") String idCallRote,
+            String nome,
+            String cpf,
+            String idCallRote,
             int pontuacao,
-            @NotBlank String role,
+            String role,
             Long version
     ) {
         super(nome, cpf, idCallRote, pontuacao, role, version);
